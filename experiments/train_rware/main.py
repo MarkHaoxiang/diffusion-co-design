6adf560dad30e018b6daaac1f5141fb0aff4bf30
MarkHaoxiang <mark.haoxiang@gmail.com>
2025-08-23 07:19:01 +0100

    Add scenario name to log

--- experiments/train_rware/main.py
+++ experiments/train_rware/main.py
@@ -21,7 +21,7 @@ class Trainer(
     ]
 ):
     def __init__(self, cfg: schema.TrainingConfig):
-        super().__init__(cfg, "diffusion-co-design-rware")
+        super().__init__(cfg, f"diffusion-co-design-rware-{cfg.scenario_name}")
 
     def create_designer(self, scenario, designer, ppo, artifact_dir, device):
         return design.create_designer(
